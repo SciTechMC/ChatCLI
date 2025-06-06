6df7be25bcba848c18620605b6dba421f2c3663b
SciTechMC <jan.vdc@gmail.com>
2025-06-06 15:04:10 +0200

    update electron app layout and features

--- client/electronapp/api.js
+++ client/electronapp/api.js
@@ -1,6 +1,9 @@
 const API_BASE = "http://localhost:5123";
 
-export const API_URLS = {
+window.API_URLS = {
   VERIFY_CONNECTION: `${API_BASE}/verify-connection`,
-  
+  USER_LOGIN: `${API_BASE}/user/login`,
+  USER_REGISTER: `${API_BASE}/user/register`,
+  USER_RESET_PASSWORD_REQUEST: `${API_BASE}/user/reset-password-request`,
+  // add more as needed
 };
