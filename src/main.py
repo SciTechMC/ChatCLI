9566b2ee355a5d2b63851c654616a9971cddb739
SciTechMC <jan.vdcg@gmail.com>
2025-07-12 22:17:42 +0200

    entered correct flask port

--- src/main.py
+++ src/main.py
@@ -13,6 +13,6 @@ def teardown_db(exception):
 
 if __name__ == "__main__":
     try:
-        app.run(host="0.0.0.0", port=5000, debug=True)
+        app.run(host="0.0.0.0", port=5123, debug=True)
     except Exception as e:
         print(f"Error: {e}")
