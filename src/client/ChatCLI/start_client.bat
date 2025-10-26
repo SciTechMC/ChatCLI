7a838e31eaa80b50e16d9e0417a0547be8a0793d
SciTechMC <jan.vdcg@gmail.com>
2025-10-26 10:43:08 +0100

    remove unused Electron app files and scripts

--- src/client/ChatCLI/start_client.bat
+++ src/client/ChatCLI/start_client.bat
@@ -3,7 +3,7 @@ set ELECTRON_ENABLE_LOGGING=true
 set ELECTRON_DISABLE_SECURITY_WARNINGS=true
 rem Install dependencies (if you need to)
 call npm install
-
+call npx electron-builder install-app-deps --platform=win32 --arch=x64
 rem Launch the app
 call npm start
 
--- src/client/electronapp/config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-const HOST = '127.0.0.1';
-const API_PORT = 5123;
-const WS_PORT = 8765;
-
-module.exports = {
-  BASE_URL: `http://${HOST}:${API_PORT}`,
-  WS_URL: `ws://${HOST}:${WS_PORT}/ws`,
-};
